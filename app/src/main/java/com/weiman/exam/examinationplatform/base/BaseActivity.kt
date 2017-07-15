e04dda525ddae87415e488e83d710a1b074f5ac0
1374329135@qq.com <1374329135@qq.com>
2017-07-15 19:19:51 +0800

    网络封装

--- app/src/main/java/com/weiman/exam/examinationplatform/base/BaseActivity.kt
+++ app/src/main/java/com/weiman/exam/examinationplatform/base/BaseActivity.kt
@@ -1,6 +1,8 @@
 package com.weiman.exam.examinationplatform.base
 
 import android.content.Context
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.view.View
@@ -24,7 +26,6 @@ import rx.subscriptions.CompositeSubscription
  * 描述 基类
  */
 abstract class BaseActivity<T:BasePresenter<*>> :FragmentActivity(){
-
     lateinit var mBaseView:View
     lateinit var mChildView:View
     lateinit var mContext:Context
